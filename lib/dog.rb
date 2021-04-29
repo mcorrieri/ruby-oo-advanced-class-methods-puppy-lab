77f898901b5a8407ded8dcb51d783af995411223
Michael <Mlcorrieri@gmail.com>
2021-04-28 23:11:14 -0400

    commit tree 10dd6d33370182c524ce2bc1491e1176dddeb901

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
-# Add your code here
+class Dog 
+    @@all = []
+
+    attr_accessor :name 
+
+    def intialize(name)
+        @name = name
+        @all << self
+    end
+
+    def self.all
+        @@all
+    end
+end
+Dog.all
